7f6634343568c2d72ae49a79c424c678e0dbbc29
gokayozcoban <43710003+gokayozcoban@users.noreply.github.com>
2018-12-14 01:49:31 +0200

    divmod()

---
+++ divmod().py
@@ -0,0 +1,28 @@
+# divmod() Fonksiyonu
+
+# divmod() fonksiyonu iki parametreden oluşmaktadır.
+# Birinci parametre ikinci parametreye böler ve çıktı olarak
+# bölüm ve kalan ifadesini vermektedir.
+
+# divmod(12,6) ifadesi (2,0) çıktısını verir.
+# 12’nin 6 bölünmesiyle 2 elde edilir, kalan ise 0’dır.
+# divmod() fonksiyonu her zaman tamsayı çıktısı vermektedir.
+
+
+
+sayi1= divmod(14,3)
+
+print(sayi1)
+
+
+
+
+
+sayi1= divmod(17,7)
+
+sayi2= divmod(6,5)
+
+sayi3= divmod(13,6)
+
+print(sayi1,sayi2,sayi3)
+
